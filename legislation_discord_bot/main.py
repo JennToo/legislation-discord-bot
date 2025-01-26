411bc207e40f195e4042f8fed8574738aecf0d77
Jennifer Wilcox <jennifer@nitori.org>
2025-01-26 19:52:12 +0000

    Add management commands for config

--- legislation_discord_bot/main.py
+++ legislation_discord_bot/main.py
@@ -11,11 +11,12 @@ import aiojobs
 from . import bills
 
 ALLOWED_CHANNELS = [
-    535528612502437889,  # Testing channel
-    # 1181424039756050514,  # Trans North AL channel
-    # 1193332824846127275,  # readfree
-    # 1206065865485979689,  # LGBTQ+ Action Group
+    # 535528612502437889,  # Testing channel
+    1181424039756050514,  # Trans North AL channel
+    1193332824846127275,  # readfree
+    1206065865485979689,  # LGBTQ+ Action Group
 ]
+ADMIN_ID = 258068291128524802
 MESSAGE_SEND_COOLDOWN = 15
 FULL_SCAN_INTERVAL = 15 * 60
 
@@ -75,13 +76,41 @@ tree = discord.app_commands.CommandTree(client)
 
 @tree.command(name="status", description="Status of bills-of-interest")
 async def status_command(interaction):
+    config = bills.load_config()
     bill_db = bills.load_bill_database()
-    summary = bills.render_bills_summary(bill_db)
+    summary = bills.render_bills_summary(bill_db, config)
     db_update = datetime.datetime.fromtimestamp(
         bills.BILL_DATABASE_FILE.stat().st_mtime, tz=datetime.timezone.utc
     )
 
     message = f"## Status of Bills of Interest\n{summary}_Last DB Update: {db_update.isoformat()}Z_"
+    await interaction.response.send_message(message)
+
+
+@tree.command(name="mark", description="Mark a bill for following")
+async def mark(interaction, bill: str):
+    config = bills.load_config()
+    if interaction.user.id != ADMIN_ID:
+        message = "Sorry, only Jen can use this command"
+    else:
+        if bill not in config["bills-of-interest"]:
+            config["bills-of-interest"].append(bill)
+        message = f"OK, new bills are: {config['bills-of-interest']}"
+        bills.save_config(config)
+    await interaction.response.send_message(message)
+
+
+@tree.command(name="unmark", description="Unmark a bill for following")
+async def unmark(interaction, bill: str):
+    config = bills.load_config()
+    if interaction.user.id != ADMIN_ID:
+        message = "Sorry, only Jen can use this command"
+    else:
+        config["bills-of-interest"] = [
+            x for x in config["bills-of-interest"] if x != bill
+        ]
+        message = f"OK, new bills are: {config['bills-of-interest']}"
+        bills.save_config(config)
     await interaction.response.send_message(message)
 
 
